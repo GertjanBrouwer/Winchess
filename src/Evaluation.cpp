f6e45ab3203a7b4ed1a60ad371a392b9a8dc5c81
Henrico Pops <henricopops@gmail.com>
2020-06-09 16:17:45 +0200

    Improved evaluation (#49)

--- src/Evaluation.cpp
+++ src/Evaluation.cpp
@@ -5,6 +5,8 @@
 #endif
 #include <sstream>
 
+#include "MoveGeneration.h"
+
 inline int bitCount(bitboard board)
 {
 #if _WIN32
@@ -17,19 +19,39 @@ inline int bitCount(bitboard board)
 
 int Evaluation::GetPieceBasedEvaluation(Board* board)
 {
-	int total = 0;
-	unsigned int whiteTotal = 0;
-	unsigned int blackTotal = 0;
-
-	whiteTotal = GetPieceBasedEvaluationOfColor(board, White);
-	blackTotal = GetPieceBasedEvaluationOfColor(board, Black);
+	unsigned int whiteTotal = GetPieceBasedEvaluationOfColor(board, White);
+	whiteTotal += GetBoardEvaluationOfColor(board, White);
+	unsigned int blackTotal = GetPieceBasedEvaluationOfColor(board, Black);
+	blackTotal += GetBoardEvaluationOfColor(board, Black);
 
 	//evaluate count and determine
+	int total = 0;
 	total += whiteTotal;
 	total -= blackTotal;
 
 	//return count 
 	return (board->turn == White ? total : -total);
+}
+
+int Evaluation::GetBoardEvaluationOfColor(Board* board, PieceColor color)
+{
+	int total = 0;
+	// Bishop pair
+	if (bitCount(board->pieces[color][Bishop]) >= 2)
+		total += 50;
+
+	for (auto i = 0; i < 6; i++)
+	{
+		auto pieces = board->pieces[color][i];
+		while (pieces)
+		{
+			short position = MoveGeneration::getBitIndex(pieces);
+			// Remove piece from allPieces
+			pieces &= ~((bitboard)1 << position);
+			total += psqt[i][position];
+		}
+	}
+	return total;
 }
 
 int Evaluation::GetPieceBasedEvaluationOfColor(Board* board, PieceColor color)
